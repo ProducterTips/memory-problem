067cd6355e71b19e803e4d213be821125404834b
kevinzhow <kevinchou.c@gmail.com>
2015-10-08 00:44:10 +0800

    first commit

---
+++ MemoryProblem/BadGuy.swift
@@ -0,0 +1,13 @@
+//
+//  BadGuy.swift
+//  MemoryProblem
+//
+//  Created by zhowkevin on 15/10/8.
+//  Copyright © 2015年 zhowkevin. All rights reserved.
+//
+
+import UIKit
+
+class BadGuy: NSObject {
+
+}
---
+++ MemoryProblem/GoodViewController.swift
@@ -0,0 +1,35 @@
+//
+//  GoodViewController.swift
+//  MemoryProblem
+//
+//  Created by zhowkevin on 15/10/8.
+//  Copyright © 2015年 zhowkevin. All rights reserved.
+//
+
+import UIKit
+
+class GoodViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
